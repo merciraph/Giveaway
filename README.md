c62cc7f6d8ff2f7b8668e35a9b0c8dfbbe2223af
Tim <tim.branstett@gmail.com>
2025-06-26 12:19:22 +0200

    Temp testgit ignore (#2)
    * ignore all except csv
    
    * Update .gitignore sinon c la bez
    
    * update test
    
    * edit .gitignore
    
    * Synchro 26/06/2025 - 11:37
    
    * update pour merge en main
    
    * gitIgnore for main branch
    
    * update data placeholder
    
    * ajout data pour pagination

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# HELLO WORLD
+# CONTEXTE
+Projet qui traite un csv récupéré en dehors, s'il est différent de celui du repository git, le synchronise et le pousse.
+
+Interface de visualisation du csv déployé en GitHub Pages
+
+# INSTALLATION POUR WINDOWS
+- **SANS GIT INSTALLE**
+    - Créer un dossier utils à la racine du projet
+    - Télécharger [Git.Portable](https://github.com/git-for-windows/git/releases/download/v2.50.0.windows.1/PortableGit-2.50.0-64-bit.7z.exe) et l'inclure dans le dossier utils
+    - Extraire l'executable Git en double cliquant sur le fichier téléchargé
+- **AVEC GIT INSTALLÉ**
+    - Simplement remplir la variable `GITEXE=` avec la valeur `git`
+
+# PARAMETRAGE
+
+- Remplir le fichier `config.bat` avec les informations demandées
+```bat
+set PROJECTFOLDER=path/to/project/
+set GITEXE=path/to/git_portable/bin/git.exe
+set CSVFOLDER=path/to/csvfileToCopyFolder
+```
+
+# COMMENTAIRES
+
+Automatisation du projet non inclus dans cette documentation
---
+++ script.bat
@@ -0,0 +1,32 @@
+@ECHO OFF
+
+:: Vérification des variables
+ECHO %date% - %time:~0,5% --- VERIFICATION DES VARIABLES >> script.log
+IF NOT EXIST config.bat (
+    ECHO Le fichier de configuration config.bat est manquant. Veuillez le créer avant d'exécuter ce script. >> script.log
+    EXIT /B 1
+)
+call config.bat
+
+IF NOT EXIST %CSVFOLDER%file.csv (
+    ECHO %date% - %time:~0,5% Le dossier d'installation de StreamerBot est mal défini ou le fichier file.csv est manquant. >> script.log
+    EXIT /B 1
+)
+IF NOT EXIST %GITEXE% (
+    ECHO Le dossier GIT est mal défini ou le fichier git.exe est manquant. >> script.log
+    EXIT /B 1
+)
+
+IF NOT EXIST %PROJECTFOLDER% (
+    ECHO Le dossier du projet est mal défini >> script.log
+    EXIT /B 1
+)
+
+ECHO %date% - %time:~0,5% --- COPIE DU CSV >> script.log
+COPY /Y %CSVFOLDER%file.csv %PROJECTFOLDER%data\test.csv >> script.log
+ECHO %date% - %time:~0,5% --- MISE A JOUR GIT >> script.log
+CD /D %PROJECTFOLDER%
+%GITEXE% add data\test.csv >> script.log
+%GITEXE% commit -m "Synchro %date% - %time:~0,5%" >> script.log
+%GITEXE% push >> script.log
+ECHO %date% - %time:~0,5% --- FIN DU SCRIPT >> script.log

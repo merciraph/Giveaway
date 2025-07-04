0ed42d1bb65ef092af091b1a1dc109a502eec388
merciraph <merciraphpro@gmail.com>
2025-07-04 23:58:55 +0200

    Update HeaderView.vue

--- src/components/pages/HeaderView.vue
+++ src/components/pages/HeaderView.vue
@@ -4,9 +4,10 @@
             <h2>Giveaway du mois de {{ month }}</h2>
         </template>
         <template #content class="item2">
-            <Avatar image="/customGiveaway/merci_raph.jpg" size="xlarge" shape="circle"/>
+            <Avatar image="/Giveaway/merci_raph.jpg" size="xlarge" shape="circle"/>
             <p>Une fois par stream vous pouvez acheter une récompense dans la boutique de points de la chaîne. </p>
-            <p>Elle vous donne un point. Une fois par mois on tire au hasard une personne parmi les 20 qui ont le plus de points</p>
+            <p>Elle vous donne un point. Plus vous avez de points, plus vous avez de chance de gagner ! </p>
+            <p>Une fois par mois on tire au hasard une personne parmi les 50 qui ont le plus de points</p>
             <p>Cette personne gagne 20€ de RP !</p>
         </template>
     </Card>

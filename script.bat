86610f7a9c0acf1d90e6434fe2fe45ddd6c83ac9
Tim <tim.branstett@gmail.com>
2025-07-04 23:56:21 +0200

    Update script.bat

--- script.bat
+++ script.bat
@@ -12,21 +12,18 @@ IF NOT EXIST %CSVFOLDER%file.csv (
     ECHO %date% - %time:~0,5% Le dossier d'installation de StreamerBot est mal défini ou le fichier file.csv est manquant. >> script.log
     EXIT /B 1
 )
-IF NOT EXIST %GITEXE% (
-    ECHO Le dossier GIT est mal défini ou le fichier git.exe est manquant. >> script.log
-    EXIT /B 1
-)
 
 IF NOT EXIST %PROJECTFOLDER% (
     ECHO Le dossier du projet est mal défini >> script.log
     EXIT /B 1
 )
-
+ECHO %date% - %time:~0,5% --- PULL DES SOURCES >> script.log
+git pull
 ECHO %date% - %time:~0,5% --- COPIE DU CSV >> script.log
 COPY /Y %CSVFOLDER%file.csv %PROJECTFOLDER%data\test.csv >> script.log
 ECHO %date% - %time:~0,5% --- MISE A JOUR GIT >> script.log
 CD /D %PROJECTFOLDER%
-%GITEXE% add data\test.csv >> script.log
-%GITEXE% commit -m "Synchro %date% - %time:~0,5%" >> script.log
-%GITEXE% push >> script.log
+git add data\test.csv >> script.log
+git commit -m "Synchro %date% - %time:~0,5%" >> script.log
+git push >> script.log
 ECHO %date% - %time:~0,5% --- FIN DU SCRIPT >> script.log

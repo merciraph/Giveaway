01749b8b89f9bc41628a090fd2821687d33d7c15
merciraph <merciraphpro@gmail.com>
2025-07-04 13:48:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/customGiveaway/",
+  base: "/Giveaway/",
   css: {
     preprocessorOptions: {
       scss: {

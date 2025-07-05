6cea3a291b6c668b029f438597d49fb60697483e
Tim <tim.branstett@gmail.com>
2025-07-06 01:25:24 +0200

    Changement design

--- src/components/pages/HeaderView.vue
+++ src/components/pages/HeaderView.vue
@@ -1,23 +1,19 @@
 <template>
-    <Card class="card">
-        <template #title>
-            <h2>Giveaway du mois de {{ month }}</h2>
-        </template>
-        <template #content class="item1">
+    <Panel toggeable>
+        <template #header>
             <Avatar image="/Giveaway/merci_raph.jpg" size="xlarge" shape="circle"/>
-            <text class="item2">
+            <h1>Giveaway du mois de {{ month }}</h1>
+        </template>
                 <p>Une fois par stream vous pouvez acheter une récompense dans la boutique de points de la chaîne. Elle vous donne un point. Plus vous avez de points, plus vous avez de chance de gagner ! </p>
                 <p>Une fois par mois on tire au hasard une personne parmi les 50 qui ont le plus de points. Cette personne gagne 20€ de RP !</p>
-                <p> Les personnes en <b class="bold">gras et jaune</b> sont en course pour être sélectionnées pour le tirage au sort !</p>
-            </text>
-        </template>
-    </Card>
+                <p> Les personnes en <b class="bold">gras et jaune</b> sont en course pour être sélectionnées dans le tirage au sort !</p>
+    </Panel>
 </template>
 
 
 <script setup>
-import { Card } from 'primevue';
 import Avatar from 'primevue/avatar';
+import Panel from 'primevue/panel';
 
 const month = new Date().toLocaleString('default', { month: 'long' }).charAt(0).toUpperCase() + new Date().toLocaleString('default', { month: 'long' }).slice(1);
 </script>

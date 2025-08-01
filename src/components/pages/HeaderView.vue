5a8b4d9c400e69fd0cc95156f75ef6c63c998bec
merciraph <merciraphpro@gmail.com>
2025-08-01 13:25:59 +0200

    Update HeaderView.vue

--- src/components/pages/HeaderView.vue
+++ src/components/pages/HeaderView.vue
@@ -5,7 +5,7 @@
             <h1>Giveaway du mois de {{ month }}</h1>
         </template>
                 <p>Une fois par stream vous pouvez acheter une récompense dans la boutique de points de la chaîne. Elle vous donne un point. Plus vous avez de points, plus vous avez de chance de gagner ! </p>
-                <p>Une fois par mois on tire au hasard une personne parmi les 50 qui ont le plus de points. Cette personne gagne 20€ de RP !</p>
+                <p>Une fois par mois on tire au hasard une personne parmi les 40 qui ont le plus de points. Cette personne gagne 20€ de RP !</p>
                 <p> Les personnes en <b class="bold">gras et jaune</b> sont en course pour être sélectionnées dans le tirage au sort !</p>
     </Panel>
 </template>

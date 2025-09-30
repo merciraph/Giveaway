ef62803516083091a98a2feaa9f4c4d46cfbfabc
Tim Branstett <tim.branstett@gmail.com>
2025-09-30 15:15:18 +0200

    ajouts tabs

--- src/components/pages/HeaderView.vue
+++ src/components/pages/HeaderView.vue
@@ -2,13 +2,39 @@
     <Panel toggeable style="margin: 5px">
         <template #header>
             <Avatar image="/Giveaway/merci_raph.jpg" size="xlarge" shape="circle"/>
-            <h1>Giveaway du mois de {{ month }}</h1>
+            <Tabs value="0">
+                <TabList>
+                    <Tab value="0">
+                        <h1>Giveaway du mois de {{ month }}</h1>
+                    </Tab>
+                    <Tab value="1"><h1>Liste des précédents gagnants</h1></Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel value="0">
+                        <p>Une fois par stream vous pouvez acheter une récompense dans la boutique de points de la chaîne. Elle vous donne un point. Plus vous avez de points, plus vous avez de chance de gagner ! </p>
+                        <p>Une fois par mois on tire au hasard une personne parmi les 40 qui ont le plus de points. Cette personne gagne 20€ de RP !</p>
+                        <p> Les personnes en <b class="bold">gras et jaune</b> sont en course pour être sélectionnées dans le tirage au sort !</p>
+                    </TabPanel>
+                    <TabPanel value="1">
+                        <p> Les gagnants des mois précédents sont :</p>
+                        <ul>
+                            <li>
+                                Juin : adjectifs
+                            </li>
+                            <li>
+                                Juillet : Mugi_KillerPigs
+                            </li>
+                            <li>
+                                Août : Viciouso
+                            </li>
+                            <li>
+                                Septembre : Anjalbert
+                            </li>
+                        </ul>
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
         </template>
-                <p>Une fois par stream vous pouvez acheter une récompense dans la boutique de points de la chaîne. Elle vous donne un point. Plus vous avez de points, plus vous avez de chance de gagner ! </p>
-                <p>Une fois par mois on tire au hasard une personne parmi les 40 qui ont le plus de points. Cette personne gagne 20€ de RP !</p>
-                <p> Les personnes en <b class="bold">gras et jaune</b> sont en course pour être sélectionnées dans le tirage au sort !</p>
-                
-                <p>Les gagnants des mois précédents sont : adjectifs, Mugi Killer Pigs, Viciouso et Anjalbert !</p>
     </Panel>
 </template>
 
@@ -16,6 +42,11 @@
 <script setup>
 import Avatar from 'primevue/avatar';
 import Panel from 'primevue/panel';
+import Tabs from 'primevue/tabs';
+import TabList from 'primevue/tablist';
+import Tab from 'primevue/tab';
+import TabPanels from 'primevue/tabpanels';
+import TabPanel from 'primevue/tabpanel';
 
 const month = new Date().toLocaleString('default', { month: 'long' }).charAt(0).toUpperCase() + new Date().toLocaleString('default', { month: 'long' }).slice(1);
 </script>

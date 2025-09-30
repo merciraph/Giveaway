645083c3fae384903408f93104527aec6a2e06a7
merciraph <merciraphpro@gmail.com>
2025-09-30 13:31:34 +0200

    Update HeaderView.vue

--- src/components/pages/HeaderView.vue
+++ src/components/pages/HeaderView.vue
@@ -7,6 +7,8 @@
                 <p>Une fois par stream vous pouvez acheter une récompense dans la boutique de points de la chaîne. Elle vous donne un point. Plus vous avez de points, plus vous avez de chance de gagner ! </p>
                 <p>Une fois par mois on tire au hasard une personne parmi les 40 qui ont le plus de points. Cette personne gagne 20€ de RP !</p>
                 <p> Les personnes en <b class="bold">gras et jaune</b> sont en course pour être sélectionnées dans le tirage au sort !</p>
+                
+                <p>Les gagnants des mois précédents sont : adjectifs, Mugi Killer Pigs, Viciouso et Anjalbert !</p>
     </Panel>
 </template>
 

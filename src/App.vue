10176692cac83e4367d340f9bc32b8cf18885518
Tim <tim.branstett@gmail.com>
2025-11-12 16:05:11 +0100

    update style

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
 import HeaderView from './components/pages/HeaderView.vue';
 import TableView from './components/pages/TableView.vue';
 import FooterView from './components/pages/FooterView.vue';
+import Image from 'primevue/image';
 
 </script>
 
 <template>
   <div class="main-container">
     <div class="header-container">
-        <HeaderView />
+      <Image class="avatar" image-style="border-radius:50%" src="/Giveaway/merci_raph.jpg"/>
+      <HeaderView />
     </div>
     <div class="table-container">
       <TableView />
--- src/components/pages/HeaderView.vue
+++ src/components/pages/HeaderView.vue
@@ -1,17 +1,25 @@
 <template>
-    <Panel toggeable style="margin: 5px">
+    <Panel toggeable unstyled="true" class="header-container">
         <Tabs value="0">
             <TabList>
                 <Tab value="0">Règles</Tab>
                 <Tab value="1">Précédents gagnants</Tab>
             </TabList>
             <TabPanels>
-                <TabPanel value="0">
-                    <RulesView />
-                </TabPanel>
-                <TabPanel value="1">
-                    <PreviousWinnerView />
-                </TabPanel>
+                <ScrollPanel style="width: 100%; height: 15vh"
+                    :dt="{
+                        bar: {
+                            background: '{primary.color}'
+                        }
+                    }"
+                >
+                    <TabPanel value="0">
+                        <RulesView />
+                    </TabPanel>
+                    <TabPanel value="1">
+                        <PreviousWinnerView />
+                    </TabPanel>
+                </ScrollPanel>
             </TabPanels>
         </Tabs>
     </Panel>
@@ -26,4 +34,5 @@
     import RulesView from './RulesView.vue';
     import PreviousWinnerView from './PreviousWinnersView.vue';
     import Panel from 'primevue/panel';
+    import ScrollPanel from 'primevue/scrollpanel';
 </script>
